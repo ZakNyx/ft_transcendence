5e4e6b8b60e6fcaafbc1397fc29866a311a432d2
Zakaria Ihirri <zihirri@e1r4p8.1337.ma>
2023-08-03 20:03:51 +0100

    Gradient colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
         float: {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-20px)' },
-        },
+        },  
       },
       animation: {
         float: 'float 3s ease-in-out infinite', // Adjust the animation duration and easing for a smoother effect
