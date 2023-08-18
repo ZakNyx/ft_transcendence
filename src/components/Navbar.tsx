2a439da5243f0ee3b99164ee34550c1b34c4a261
Zakaria Ihirri <zihirri@c4r2p2.1337.ma>
2023-08-18 20:09:16 +0100

    Made drop down hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,34 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [dropdownTimeout, setDropdownTimeout] = useState<number | undefined>(undefined);
 
   const dropdownRef = useRef<HTMLDivElement | null>(null);
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const openDropdown = () => {
+    clearTimeout(dropdownTimeout);
+    setIsDropdownOpen(true);
+  };
+
+  const closeDropdown = () => {
+    const timeout = setTimeout(() => {
+      setIsDropdownOpen(false);
+    }, 300); // Adjust the delay time as needed
+    setDropdownTimeout(timeout);
   };
 
   useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        setIsDropdownOpen(false);
-      }
-    };
-
-    if (isDropdownOpen) {
-      document.addEventListener("mousedown", handleOutsideClick);
-    } else {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    }
-
     return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
+      clearTimeout(dropdownTimeout);
     };
-  }, [isDropdownOpen]);
+  }, [dropdownTimeout]);
+
   return (
     <header>
       <style>
-        @import
-        url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
       </style>
       <nav className="navbar flex justify-between items-center w-auto mx-auto top-0">
         <div className="relative">
@@ -80,52 +77,57 @@ function NavBar() {
               </NavLink>
             </li>
             <li className="flex items-center">
-              <img
-                src="../../public/images/zihirri.jpg"
-                alt="profile picture"
-                className="w-12 h-12 cursor-pointer rounded-[30px] object-contain flex-shrink-0 min-w-[48px] min-h-[48px]"
-                onClick={toggleDropdown}
-              />
-              {isDropdownOpen && (
-                <div
+            <div
+                className="relative"
+                onMouseEnter={openDropdown}
+                onMouseLeave={closeDropdown}
+              >
+                <img
+                  src="../../public/images/zihirri.jpg"
+                  alt="profile picture"
+                  className="w-12 h-12 cursor-pointer rounded-[30px] object-contain flex-shrink-0 min-w-[48px] min-h-[48px]"
+                />
+                {isDropdownOpen && (
+                  <div
                   ref={dropdownRef}
-                  className="absolute right-0 z-10 mt-2 w-40  text-left origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 opacity-70 focus:outline-none "
-                  style={{ top: "4.5rem", right: "0rem" }}
-                >
-                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-                    <li className="px-4 text-gray-700 hover:bg-gray-200 flex items-center ">
-                      <img
-                        className="w-4 h-4 mr-2"
-                        src="../../public/images/user.svg"
-                        alt="User"
-                      />
-                      <NavLink to="/profile" className="block">
-                        Profile
-                      </NavLink>
-                    </li>
-                    <li className="px-4 text-gray-700 hover:bg-gray-200 flex items-center ">
-                      <img
-                        className="w-4 h-4 mr-2"
-                        src="../../public/images/setting.svg"
-                        alt="User"
-                      />
-                      <NavLink to="/settings" className="block">
-                        Settings
-                      </NavLink>
-                    </li>
-                    <li className="px-4 text-gray-700 hover:bg-gray-200 flex items-center ">
-                      <img
-                        className="w-4 h-4 mr-2"
-                        src="../../public/images/logout.svg"
-                        alt="logout"
-                      />
-                      <NavLink to="/" className="block">
-                        Logout
-                      </NavLink>
-                    </li>
-                  </ul>
-                </div>
-              )}
+                    className="absolute right-0 z-10 mt-2 w-40 text-left origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 opacity-70 focus:outline-none"
+                    style={{ top: "3.5rem", right: "-2.5rem" }}
+                  >
+                    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                      <li className="px-4 text-gray-700 hover:bg-gray-200 flex items-center">
+                        <img
+                          className="w-4 h-4 mr-2"
+                          src="../../public/images/user.svg"
+                          alt="User"
+                        />
+                        <NavLink to="/profile" className="block">
+                          Profile
+                        </NavLink>
+                      </li>
+                      <li className="px-4 text-gray-700 hover:bg-gray-200 flex items-center">
+                        <img
+                          className="w-4 h-4 mr-2"
+                          src="../../public/images/setting.svg"
+                          alt="User"
+                        />
+                        <NavLink to="/settings" className="block">
+                          Settings
+                        </NavLink>
+                      </li>
+                      <li className="px-4 text-gray-700 hover:bg-gray-200 flex items-center">
+                        <img
+                          className="w-4 h-4 mr-2"
+                          src="../../public/images/logout.svg"
+                          alt="logout"
+                        />
+                        <NavLink to="/" className="block">
+                          Logout
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
             </li>
           </ul>
         </div>
