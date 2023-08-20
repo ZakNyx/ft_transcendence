8b2c098fcfc5897e3d4b51d11c28ba408347d56e
Zakaria Ihirri <zihirri@c4r2p2.1337.ma>
2023-08-20 10:44:01 +0100

    Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,8 +4,26 @@ import React from "react";
 function Profile() {
   return (
     <div className="background-image">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;500&display=swap');
+      </style>
       <NavBar />
-      <h1>Hello You landed on Profile page </h1>
+      <div className="bg-black rounded-[155px] w-[911px] h-[450px] flex items-center p-6 ml-7 mt-7">
+        {" "}
+        <div className="flex items-center">
+          <img
+            src="../../public/images/zihirri.jpg"
+            alt="profile image"
+            className="w-52 h-50 rounded-[7rem] mr-6 md:mr-3 lg:mr-6 ml-[35px]"
+          />
+          <div className="  ">
+            <h1 className="text-white font-[Rubik] text-4xl">Zihirri</h1>
+            <h2 className="text-white font-[Rubik] text-2xl">Elo - 1000</h2>
+            <h2 className="text-white font-[Rubik] text-2xl">Rank - #667</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
