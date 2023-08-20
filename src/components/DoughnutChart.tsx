65144e74402b3e6fd181b65fc0b0a6ece6204d45
Zakaria Ihirri <zihirri@c4r3p2.1337.ma>
2023-08-20 19:20:16 +0100

    Trying to create a piechart didnt work (y)

---
+++ src/components/DoughnutChart.tsx
@@ -0,0 +1,9 @@
+
+function DoughnutChart() {
+  return(
+    <div>
+      <h1 className="text-white">Allah-Las</h1>
+    </div>
+  )
+} 
+export default DoughnutChart;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import NavBar from "../components/Navbar";
-import React from "react";
+import DoughnutChart from "../components/DoughnutChart"
 
 function Profile() {
   return (
@@ -10,7 +10,6 @@ function Profile() {
       </style>
       <NavBar />
       <div className="bg-black rounded-[155px] w-[911px] h-[450px] flex items-center p-6 ml-7 mt-7">
-        {" "}
         <div className="flex items-center">
           <img
             src="../../public/images/zihirri.jpg"
@@ -19,13 +18,30 @@ function Profile() {
           />
           <div className="  ">
             <h1 className="text-white font-[Rubik] text-4xl">Zihirri</h1>
-            <h2 className="text-white font-[Rubik] text-2xl">Elo - 1000</h2>
-            <h2 className="text-white font-[Rubik] text-2xl">Rank - #667</h2>
+            <h2 className="text-white font-[Rubik] text-2xl flex items-center">
+              <img
+                className="w-6 h-6 mr-2"
+                src="../public/images/trophy.png"
+                alt="42 Logo"
+              />
+              Elo - 1000
+            </h2>
+            <h2 className="text-white font-[Rubik] text-2xl flex items-center">
+              <img
+                className="w-6 h-6 mr-2"
+                src="../public/images/rank.png"
+                alt="42 Logo"
+              />
+              Rank - #667
+            </h2>
           </div>
         </div>
+        <div>
+        <DoughnutChart />
+      </div>
       </div>
     </div>
-  );
+);
 }
 
 export default Profile;
