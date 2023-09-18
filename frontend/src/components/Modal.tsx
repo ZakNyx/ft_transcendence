6f3d8ad00e00954401718ab82fbb2125a311905a
Zakaria Ihirri <gobuwu@hotmail.com>
2023-09-18 19:40:56 +0100

    Done with the 2FA modals styling

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -8,52 +8,88 @@ interface ModalProps {
 }
 
 function Modal({ isChecked, setIsChecked }: ModalProps) {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const handleActive = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleClick = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div>
       <Popup
         trigger={
-          <button className="button relative p-2 bg-blue-500 hover:bg-blue-600 hover:cursor-pointer text-white w-auto rounded-lg flex items-center">
-            Open Modal
+          <button
+            className={`button relative p-2 ${
+              isActive
+                ? "bg-red-500 hover:bg-red-600"
+                : "bg-blue-500 hover:bg-blue-600"
+            }  hover:cursor-pointer text-gray-200 w-auto rounded-lg flex items-center transition`}
+          >
+            {isActive ? "Disable 2FA" : "Enable 2FA"}
           </button>
         }
         modal
         nested
       >
         {(close: () => void) => (
-          <div className="modal background-gray font-semibold text-gray-200 font-montserrat rounded-lg shadow-[0px_10px_30px_20px_#00000024] pl-4 pr-4">
+          <div className="modal background-gray  text-gray-200 font-montserrat rounded-lg shadow-[0px_10px_30px_20px_#00000024] pl-4 pr-4">
             <button className="close rounded-full text-xl ml-2" onClick={close}>
-              &times;
+              X
             </button>
             <div className="header font-bold">
               Two-Factor Authentication (2FA)
             </div>
             <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
             <div className="content">
+              {isActive ? (
+                // Render Disable 2FA content and button
+                <div>
+                  <h1 className="text-blue-300 font-bold pt-4">Disable 2FA</h1>
+                  <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                  <p className="">
+                    To disable the two-factor authentication you have to enter
+                    the password
+                  </p>
+                  <p>
+                    that appears in the Google Authentication app on your phone
+                  </p>
+                </div>
+              ) : (
+                <div>
+                  <h1 className="text-blue-300 font-bold pt-4">
+                    Configuring Google Authentication
+                  </h1>
+                  <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                  <ol>
+                    <li>
+                      Install the Google Authentication app (Android / IOS)
+                    </li>
+                    <li>In the app select the "+" Icon</li>
+                    <li>
+                      Select scan barcode and use your phone camera to scan this
+                      QR code
+                    </li>
+                  </ol>
+
+                  <div className="">
+                    <h1 className="text-blue-300 font-bold pt-4">
+                      Scan QR Code
+                    </h1>
+                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                    <img
+                      src="../../public/images/QR_Code.png"
+                      alt="QR Code"
+                      className="w-32 h-auto"
+                    />
+                  </div>
+                </div>
+              )}
               <div>
-                <h1 className="text-blue-300 font-bold">
-                  Configuring Google Authentication
-                </h1>
-                <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                <ol>
-                  <li>Install the Google Authentication app (Android / IOS)</li>
-                  <li>In the app select the "+" Icon</li>
-                  <li>
-                    Select scan barcode and use your phone camera to scan this
-                    QR code
-                  </li>
-                </ol>
-              </div>
-              <div className="">
-                <h1 className="text-blue-300 font-bold">Scan QR Code</h1>
-                <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                <img
-                  src="../../public/images/QR_Code.png"
-                  alt="QR Code"
-                  className="w-32 h-auto"
-                />
-              </div>
-              <div>
-                <h1 className="text-blue-300 font-bold">Verify Code</h1>
+                <h1 className="text-blue-300 font-bold pt-4">Verify Code</h1>
                 <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
                 <input
@@ -65,10 +101,18 @@ function Modal({ isChecked, setIsChecked }: ModalProps) {
                 <button
                   type="submit"
                   id="submit"
-                  className="bg-blue-500 text-gray-200 rounded-md px-4 py-2 cursor-pointer hover:bg-blue-600 transition"
-                  onClick={close}
+                  className={`button relative p-2 ${
+                    isActive
+                      ? "bg-red-500 hover:bg-red-600"
+                      : "bg-blue-500 hover:bg-blue-600"
+                  }  hover:cursor-pointer text-gray-200 w-auto rounded-lg flex items-center transition mb-4`}
+                  onClick={() => {
+                    handleActive();
+                    console.log(isActive);
+                    close();
+                  }}
                 >
-                  Submit
+                  {isActive ? "Disable" : "Enable"}
                 </button>
               </div>
             </div>
