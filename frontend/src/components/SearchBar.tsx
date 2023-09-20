f71494050f416a73047a75978d89d790a3be5d94
Zakaria Ihirri <gobuwu@hotmail.com>
2023-09-20 20:34:38 +0100

    Finished styling the searchbar and merging it with the backend

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,12 +1,26 @@
-import React, { useState, ChangeEvent, useEffect } from "react";
+import React, { useState, ChangeEvent, useEffect, useRef } from "react";
 import axios from "axios";
 
+interface UsersData {
+  username: string;
+  displayname: string;
+}
+
 export default function SearchBar() {
-  const [searchQuery, setSearch] = useState("");
-  const [users, setUsers] = useState([]);
+  const [searchQuery, setSearch] = useState<string>("");
+  const [users, setUsers] = useState<UsersData[] | null>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const searchInputRef = useRef<HTMLInputElement | null>(null);
+
+  const clearInput = () => {
+    setSearch("");
+    setUsers(null);
+    if (searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  };
 
   useEffect(() => {
-    // Define an async function for fetching user data
     const fetchUserData = async () => {
       const tokenCookie = document.cookie
         .split("; ")
@@ -22,7 +36,7 @@ export default function SearchBar() {
               headers: {
                 Authorization: `Bearer ${token}`,
               },
-            },
+            }
           );
           console.log(response.data);
           setUsers(response.data);
@@ -32,42 +46,93 @@ export default function SearchBar() {
       }
     };
 
-    // Trigger the fetchUserData function when searchQuery changes and it meets a length criteria
-    if (searchQuery.length >= 3) {
+    if (searchQuery.length > 0) {
       fetchUserData();
+    } else {
+      // Clear the search results when the search query is empty
+      setUsers(null);
     }
-  }, [searchQuery]); // Declare searchQuery as a dependency
+  }, [searchQuery]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const query = e.target.value;
     setSearch(query);
   };
 
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleEscapeKey = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      clearInput();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleEscapeKey);
+    return () => {
+      window.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, []);
+
   return (
-    <form className="md:mr-12 w-[20vw]">
-      <div className="relative">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-full outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
-          value={searchQuery}
-          onChange={handleInputChange}
+    <div className="relative">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         />
-      </div>
-    </form>
+      </svg>
+      <input
+        type="text"
+        placeholder="Search"
+        className={`w-full py-3 pl-12 pr-4 text-gray-500 border rounded-full ${
+          searchQuery ? 'focus:rounded-t-3xl focus:rounded-b-none' : ''
+        } outline-none bg-gray-50`}
+        value={searchQuery}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        ref={searchInputRef}
+      />
+      {searchQuery && (
+        <button
+          onClick={clearInput}
+          className="absolute top-0 right-0 h-full px-3 py-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+        >
+          X
+        </button>
+      )}
+      { users && (
+        <div className="w-full z-[1000]  rounded-t-md rounded-b-3xl bg-white absolute top-full shadow-lg border border-gray-300 p-2">
+          {users.map((item) => (
+            <div
+              key={item.username}
+              className="px-4 py-2 hover:bg-gray-100 cursor-pointer rounded-lg "
+            >
+              <a
+                href={`/profile/${item.username}`}
+                className="font-semibold font-montserrat text-npc-gray cursor-pointer"
+              >
+                {item.displayname}
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -1,13 +1,23 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../components/Navbar";
 import axios from "axios";
-import BackToTop from "../components/BackToTop.tsx";
-export default function Leaderboard() {
+import BackToTop from "../components/BackToTop";
 
-  const [data, setData] = useState(null);
+interface UserData {
+  userID: string;
+  username: string;
+  profilePicture: string;
+  displayname: string;
+  gamesPlayed: number;
+  wins: number;
+  winrate: number;
+  elo: number;
+}
+
+export default function Leaderboard() {
+  const [data, setData] = useState<UserData[] | null>(null);
 
   useEffect(() => {
-    // Function to fetch user data and set it in the state
     const fetchUserData = async () => {
       const tokenCookie = document.cookie
         .split("; ")
@@ -17,30 +27,27 @@ export default function Leaderboard() {
         const token = tokenCookie.split("=")[1];
 
         try {
-          // Configure Axios to send the token in the headers
-          const response = await axios.get("http://localhost:3000/user/leaderboard/", {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
+          const response = await axios.get<UserData[]>(
+            "http://localhost:3000/user/leaderboard/",
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
 
-          // Set the user data in the state
-          console.log(response.data);
           setData(response.data);
         } catch (error) {
-          // Handle errors gracefully (e.g., display an error message to the user)
           console.error("Error fetching user data:", error);
         }
       }
     };
 
-    // Call the fetchUserData function
     fetchUserData();
   }, []);
 
   useEffect(() => {
-    // Function to fetch user picture
-    const fetchUserPicture = async (username) => {
+    const fetchUserPicture = async (username: string) => {
       const tokenCookie = document.cookie
         .split("; ")
         .find((cookie) => cookie.startsWith("token="));
@@ -48,14 +55,14 @@ export default function Leaderboard() {
       try {
         if (tokenCookie) {
           const token = tokenCookie.split("=")[1];
-          const response = await axios.get(
+          const response = await axios.get<string>(
             `http://localhost:3000/profile/ProfilePicture/${username}`,
             {
               responseType: "arraybuffer",
               headers: {
                 Authorization: `Bearer ${token}`,
               },
-            },
+            }
           );
 
           const contentType = response.headers["content-type"];
@@ -64,34 +71,28 @@ export default function Leaderboard() {
 
           return imageUrl;
         } else {
-          // Handle the case when there is no token (e.g., display a placeholder image)
           return "URL_OF_PLACEHOLDER_IMAGE";
         }
       } catch (error) {
-        // Handle errors gracefully (e.g., display an error message to the user)
         console.error("Error fetching user picture:", error);
-        // Return the URL of a placeholder image or handle the error as needed
         return "URL_OF_PLACEHOLDER_IMAGE";
       }
     };
 
-    // Function to update profile pictures in the user data
     const updateUserPictures = async () => {
       if (data) {
-        const updateddata = [];
+        const updatedData: UserData[] = [];
 
         for (const user of data) {
           const imageUrl = await fetchUserPicture(user.username);
           user.profilePicture = imageUrl;
-          updateddata.push(user);
+          updatedData.push(user);
         }
 
-        // Update the state with the updated data
-        setData(updateddata);
+        setData(updatedData);
       }
     };
 
-    // Call the function to update user pictures
     updateUserPictures();
   }, [data]);
 
@@ -103,79 +104,81 @@ export default function Leaderboard() {
       </h1>
       <BackToTop />
       <div className="p-6 mt-6 mx-auto lg:max-w-[90%]">
-        {data && data.map((item, index) => (
-          <div
-            key={item.userID}
-            className="flex rounded-2xl p-4 mb-4 justify-between bg-npc-gray shadow-[0px_2px_4px_2px_#00000006]"
-          >
-            <div className="flex flex-col justify-center mr-4">
-              <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
-                #{index + 1}
-              </h2>
-              <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
-                Rank
-              </p>
-            </div>
+        {data &&
+          data.map((item, index) => (
+            <div
+              key={item.userID}
+              className="flex rounded-2xl p-4 mb-4 justify-between bg-npc-gray shadow-[0px_2px_4px_2px_#00000006]"
+            >
+              <div className="flex flex-col justify-center mr-4">
+                <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
+                  #{index + 1}
+                </h2>
+                <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
+                  Rank
+                </p>
+              </div>
 
-            <div className="flex items-center justify-center  mr-4">
-            {item.profilePicture && (
+              <div className="flex items-center justify-center  mr-4">
+                {item.profilePicture && (
                   <img
                     src={item.profilePicture}
                     alt="profile picture"
                     className="w-8 md:w-10 lg:w-16 xl:w-20 h-8 md:h-10 lg:h-16 xl:h-20 rounded-full"
                   />
                 )}
-              <h1 className="font-montserrat font-bold text-white ml-4 text-xs md:text-sm lg:text-base xl:text-xl">
-                {item.displayname}
-              </h1>
-            </div>
+                <h1 className="font-montserrat font-bold text-white ml-4 text-xs md:text-sm lg:text-base xl:text-xl">
+                  {item.displayname}
+                </h1>
+              </div>
 
-            <div className="hidden sm:flex flex-col justify-center mr-4">
-              <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
-                {item.gamesPlayed}
-              </h2>
-              <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
-                Games Played
-              </p>
-            </div>
+              <div className="hidden sm:flex flex-col justify-center mr-4">
+                <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
+                  {item.gamesPlayed}
+                </h2>
+                <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
+                  Games Played
+                </p>
+              </div>
 
-            <div className="hidden sm:flex flex-col justify-center mr-4">
-              <h2 className=" font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
-                {item.wins}
-              </h2>
-              <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
-                Games Won
-              </p>
-            </div>
+              <div className="hidden sm:flex flex-col justify-center mr-4">
+                <h2 className=" font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
+                  {item.wins}
+                </h2>
+                <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
+                  Games Won
+                </p>
+              </div>
 
-            <div className="flex flex-col justify-center mr-4">
-              <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
-                {item.winrate}%
-              </h2>
-              <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
-                Winrate
-              </p>
-            </div>
+              <div className="flex flex-col justify-center mr-4">
+                <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
+                  {item.winrate}%
+                </h2>
+                <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
+                  Winrate
+                </p>
+              </div>
 
-            <div className="flex flex-col justify-center">
-              <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
-                {item.elo}
-              </h2>
-              <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
-                Elo
-              </p>
-            </div>
+              <div className="flex flex-col justify-center">
+                <h2 className="font-montserrat font-semibold text-white text-sm md:text-base lg:text-xl xl:text-2xl">
+                  {item.elo}
+                </h2>
+                <p className="font-montserrat text-npc-light-gray text-xs md:text-xs lg:text-sm xl:text-base">
+                  Elo
+                </p>
+              </div>
 
-            <div className="flex flex-col justify-center ">
-              <a href="">
-                <img
-                  src="../../public/images/greaterthan.svg"
-                  className="w-8 md:w-10 lg:w-12 xl:w-12 h-8 md:h-10 lg:h-12 xl:h-12"
-                ></img>
-              </a>
+              <div className="flex flex-col justify-center ">
+                <a href="">
+                  <img
+                    src="../../public/images/greaterthan.svg"
+                    className="w-8 md:w-10 lg:w-12 xl:w-12 h-8 md:h-10 lg:h-12 xl:h-12"
+                    alt="greater than symbol"
+                  ></img>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
