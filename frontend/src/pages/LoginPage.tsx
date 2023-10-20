5128a1bc0af1e26fade7c3918bbde8fb9ac13860
Zakaria IHIRRI <zak@192.168.1.17>
2023-10-20 12:19:33 +0100

    Deleting files

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -5,8 +5,8 @@ import { useNavigate, useParams } from "react-router-dom";
 export default function LoginPage() {
   const navigate = useNavigate();
   const tokenCookie = document.cookie
-        .split("; ")
-        .find((cookie) => cookie.startsWith("token="));
+    .split("; ")
+    .find((cookie) => cookie.startsWith("token="));
   console.log(tokenCookie);
   useEffect(() => {
     if (tokenCookie) {
@@ -41,10 +41,7 @@ export default function LoginPage() {
         <h1 className="text-npc-purple text-base sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-8 text-center whitespace-nowrap">
           Play PONG with your friends
         </h1>
-        <a
-          href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-fc5e1f6b7adf4e8a71272ed39555392c3ab4980710ae3924ec3bcec7462509b9&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fredirect&response_type=code
-"
-        >
+        <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-1673790452bf538aaef1e49f04a19251ad49e71f28904d907a7039619261c115&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fredirect&response_type=code">
           <button className="relative bg-npc-purple hover:bg-purple-hover hover:translate-y-[-6px] transition-all active:bg-purple-active hover:cursor-pointer text-black font-bold shadow-but text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl sm:p-3 xl:p-4 rounded-lg flex items-center">
             {/* 42 Logo */}
             <img
