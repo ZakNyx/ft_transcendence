81def720cd6d26df84952a6392c4a3a6d0709482
JaDCHeH <jadcheh@gmail.com>
2023-11-22 13:42:43 +0100

    cleaned the code of unecessary migrations

--- backend/prisma/migrations/20231112171215_pong/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "achievements" TEXT[],
-ALTER COLUMN "status" SET DEFAULT 'ONLINE';
---
+++ backend/prisma/migrations/20231122124213_pong/migration.sql
@@ -0,0 +1,294 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ONLINE', 'OFFLINE', 'INGAME');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'OWNER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "image" TEXT,
+    "friend" JSONB[],
+    "blockedUsers" TEXT[],
+    "roomInvites" JSONB[],
+    "FirstTime" BOOLEAN NOT NULL DEFAULT true,
+    "imageUrl" TEXT NOT NULL,
+    "picture" TEXT,
+    "pictureStatus" BOOLEAN NOT NULL DEFAULT false,
+    "pictureMimetype" TEXT,
+    "username" TEXT NOT NULL,
+    "displayname" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ONLINE',
+    "wins" INTEGER NOT NULL DEFAULT 0,
+    "loses" INTEGER NOT NULL DEFAULT 0,
+    "winrate" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "gamesPlayed" INTEGER NOT NULL DEFAULT 0,
+    "elo" INTEGER NOT NULL DEFAULT 0,
+    "matchHistory" JSONB[],
+    "status2fa" BOOLEAN NOT NULL DEFAULT false,
+    "validated" BOOLEAN NOT NULL DEFAULT false,
+    "secret2fa" TEXT,
+    "secretAuthUrl" TEXT,
+    "achievements" TEXT[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" SERIAL NOT NULL,
+    "reciever" TEXT NOT NULL,
+    "sender" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "player1" TEXT NOT NULL,
+    "player2" TEXT NOT NULL,
+    "score1" TEXT NOT NULL DEFAULT '',
+    "score2" TEXT NOT NULL DEFAULT '',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ingame" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "sentAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "messageContent" TEXT NOT NULL,
+    "dmId" INTEGER NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "data" TEXT,
+    "time" TEXT,
+    "roomId" INTEGER,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DM" (
+    "id" SERIAL NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "lastUpdate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DM_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BannedUser" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "BannedUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FriendList" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "FriendList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "friendRequest" (
+    "id" SERIAL NOT NULL,
+    "senderId" INTEGER NOT NULL,
+
+    CONSTRAINT "friendRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" SERIAL NOT NULL,
+    "image" TEXT NOT NULL,
+    "RoomName" TEXT NOT NULL,
+    "visibility" TEXT NOT NULL,
+    "password" TEXT,
+    "bannedUsers" TEXT[],
+    "lastUpdate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RoomMember" (
+    "id" SERIAL NOT NULL,
+    "RoomId" INTEGER NOT NULL,
+    "memberId" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "muted" BOOLEAN NOT NULL DEFAULT false,
+    "muteExpiration" TIMESTAMP(3),
+    "joinTime" TIMESTAMP(3) NOT NULL,
+    "inviterId" TEXT,
+
+    CONSTRAINT "RoomMember_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChatRoom" (
+    "id" SERIAL NOT NULL,
+    "roomName" TEXT NOT NULL,
+    "timeCreate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "admins" TEXT[],
+    "mutedUsers" TEXT[],
+    "bannedUsers" TEXT[],
+    "isDm" BOOLEAN NOT NULL DEFAULT false,
+    "status" TEXT NOT NULL DEFAULT 'public',
+    "password" TEXT,
+
+    CONSTRAINT "ChatRoom_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_friends" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_blocked" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_friendreq" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_GamePlayers" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_participants" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_userInChatRoom" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userId_key" ON "User"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_displayname_key" ON "User"("displayname");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ChatRoom_roomName_key" ON "ChatRoom"("roomName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_friends_AB_unique" ON "_friends"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_friends_B_index" ON "_friends"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_blocked_AB_unique" ON "_blocked"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_blocked_B_index" ON "_blocked"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_friendreq_AB_unique" ON "_friendreq"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_friendreq_B_index" ON "_friendreq"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_GamePlayers_AB_unique" ON "_GamePlayers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_GamePlayers_B_index" ON "_GamePlayers"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_participants_AB_unique" ON "_participants"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_participants_B_index" ON "_participants"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_userInChatRoom_AB_unique" ON "_userInChatRoom"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_userInChatRoom_B_index" ON "_userInChatRoom"("B");
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_reciever_fkey" FOREIGN KEY ("reciever") REFERENCES "User"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_dmId_fkey" FOREIGN KEY ("dmId") REFERENCES "DM"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "MessageRoomData" FOREIGN KEY ("roomId") REFERENCES "ChatRoom"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RoomMember" ADD CONSTRAINT "RoomMember_RoomId_fkey" FOREIGN KEY ("RoomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RoomMember" ADD CONSTRAINT "RoomMember_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_friends" ADD CONSTRAINT "_friends_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_friends" ADD CONSTRAINT "_friends_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_blocked" ADD CONSTRAINT "_blocked_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_blocked" ADD CONSTRAINT "_blocked_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_friendreq" ADD CONSTRAINT "_friendreq_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_friendreq" ADD CONSTRAINT "_friendreq_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GamePlayers" ADD CONSTRAINT "_GamePlayers_A_fkey" FOREIGN KEY ("A") REFERENCES "Game"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GamePlayers" ADD CONSTRAINT "_GamePlayers_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_participants" ADD CONSTRAINT "_participants_A_fkey" FOREIGN KEY ("A") REFERENCES "DM"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_participants" ADD CONSTRAINT "_participants_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_userInChatRoom" ADD CONSTRAINT "_userInChatRoom_A_fkey" FOREIGN KEY ("A") REFERENCES "ChatRoom"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_userInChatRoom" ADD CONSTRAINT "_userInChatRoom_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
